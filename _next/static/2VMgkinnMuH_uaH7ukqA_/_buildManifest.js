87b23d81ebfbf0a02c8bffc5dcb83054c66baf67
Antti V <johhni.games@gmail.com>
2024-08-24 02:01:24 +0300

    update site with new dev version

--- _next/static/2VMgkinnMuH_uaH7ukqA_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/342-ba91e6a6c5cefcf9.js","static/chunks/pages/index-bd9b4ae71d700163.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/bdQGt13GFDVyZowHjPNtT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/55-2fbae44c3055a970.js","static/chunks/pages/index-d25e19edf542011d.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
